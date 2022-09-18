79e5c27fba77e44fd538c295fd6973ab6b301c2d
eleftheros <eleftheros_all@protonmail.com>
2022-09-18 14:21:44 +0300

    Prompt user to enter name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing game!");
+    println!("Enter your name");
+    let mut player_name = String::new();
+
+    io::stdin()
+        .read_line(&mut player_name)
+        .expect("Can't read player name.");
+    
+    println!("Your name: {}", player_name);
 }
