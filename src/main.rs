9d055d4e0494631d8478210e9c860553850f519d
eleftheros <eleftheros_all@protonmail.com>
2022-09-18 17:46:42 +0300

    Add loop with user input

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Welcome to the guessing game!");
     println!("Enter your name");
-    let mut player_name = String::new();
-
-    io::stdin()
-        .read_line(&mut player_name)
-        .expect("Can't read player name.");
+    let mut user_name = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Secret number is {}", secret_number);
     
-    println!("Your name: {}", player_name);
+    io::stdin()
+    .read_line(&mut user_name)
+    .expect("Can't read name.");
+    
+    
+    loop {
+        let mut user_guess = String::new();
+        println!("Enter your number guess ( 1-100 )");
+
+        io::stdin()
+            .read_line(&mut user_guess)
+            .expect("Can't read guess.");
+        
+        let user_guess: i32 = match user_guess.trim().parse() {
+            Ok(val) => val,
+            Err(_) => {
+                println!("Please enter valid number");
+                continue
+            }
+        };
+
+        match user_guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("You win, {}", user_name.trim());
+                break;
+            },
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small!")
+        }
+    }
 }
