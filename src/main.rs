076c9f3ba94b29772fa6e161f58b3b7230505bb7
eleftheros <eleftheros_all@protonmail.com>
2022-09-26 11:26:41 +0300

    Refactor and add colored crate

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,52 @@
-use std::io;
-use rand::Rng;
+use std::io::stdin;
+use rand::{Rng, thread_rng};
+use colored::Colorize;
 use std::cmp::Ordering;
 
 fn main() {
-    println!("Enter your name");
-    let mut user_name = String::new();
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("Secret number is {}", secret_number);
+    let mut user_name:String = String::new();
+    let secret_number: u8 = thread_rng().gen_range(1..=100);
+    println!("{}", "Welcome to the guessing game.".on_bright_cyan());
     
-    io::stdin()
-    .read_line(&mut user_name)
-    .expect("Can't read name.");
-    
+    println!("Enter your name:");
+    match stdin().read_line(&mut user_name) {
+        Ok(_) => {
+            user_name = user_name.trim().to_owned();
+            println!("Welcome, {}!", user_name.blue());
+        },
+        Err(_) => println!("Can't read name.")
+    };
     
     loop {
-        let mut user_guess = String::new();
-        println!("Enter your number guess ( 1-100 )");
+        let mut user_guess:String = String::new();
 
-        io::stdin()
-            .read_line(&mut user_guess)
-            .expect("Can't read guess.");
-        
-        let user_guess: i32 = match user_guess.trim().parse() {
-            Ok(val) => val,
-            Err(_) => {
-                println!("Please enter valid number");
-                continue
+        println!("Enter your guess (1-100) :");
+        match stdin().read_line(&mut user_guess) {
+            Ok(_) => {
+                let user_guess: u8 = match user_guess.trim().parse() {
+                    Ok(v) => {
+                        println!("You guessed {}", v);
+                        v
+                    }
+                    Err(_) => {
+                        println!("Enter valid number between 1-100.");
+                        continue;
+                    }
+                };
+
+                match user_guess.cmp(&secret_number) {
+                    Ordering::Equal => {
+                        println!("Congrats, {}, {}!", user_name.blue(), "YOU WIN".green());
+                        break;
+                    },
+                    Ordering::Greater => println!("{}", "Your guess is too high".red()),
+                    Ordering::Less => println!("{}", "Your guess is too low".red())
+                }
             }
-        };
-
-        match user_guess.cmp(&secret_number) {
-            Ordering::Equal => {
-                println!("You win, {}", user_name.trim());
-                break;
-            },
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Less => println!("Too small!")
+            Err(_) => {
+                println!("Can't read guess.");
+                continue;
+            }
         }
     }
 }
